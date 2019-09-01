2becc36e1f3845fdbd268c3db61da3985a2204c4
Maximiliano Minetto <maximinetto@gmail.com>
2019-09-01 00:01:27 -0300

    Led encendidos

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,11 +1,80 @@
 function leer(){
 	var socket = io();
-	let span = document.getElementById('mostrar');
+	
+	leerLeds();
 
-	socket.on('ver', dato => {
-		console.log(dato);
-		span.innerHTML = 'Dato: ' + dato;
-	});
+	leerPuntaje();
+
+	eventoButton();
+
+
+	function eventoButton(){
+		let cambiarEstado = document.getElementById('cambiarEstado');
+		cambiarEstado.addEventListener("click", () => {
+			let data = cambiarEstado.textContent.toLowerCase;
+			socket.emit("cambiarEstado", data);
+			socket.on("estado", data => {
+				cambiarEstado.textContent = data;
+			})
+		});
+	}
+
+	function leerPuntaje() {
+		let puntajeMaximo = document.getElementById('puntajeMaximo');
+		socket.on('puntaje', dato => {
+			console.log(dato);
+			let previousScore = Number.parseInt(puntajeMaximo.textContent, 10);
+			let currentScore = Number.parseInt(dato, 10);
+			if(previousScore < currentScore){
+				puntajeMaximo.textContent = currentScore;
+				takePhoto();
+			}
+		});
+	}
+
+	function leerLeds(){
+		let ledRojo = document.getElementById('lRojo');
+		let ledAzul = document.getElementById('lAzul');
+		let ledVerde = document.getElementById('lVerde');
+		let ledAmarillo = document.getElementById('lAmarillo');
+	
+		socket.on('ledRojo', () => {
+			let colorAnterior = "#fd4a4a";
+			ledRojo.style.backgroundColor = 'red';
+			setTimeout(() => {
+				ledRojo.style.backgroundColor = colorAnterior;
+			}, 2000);
+			
+		});
+		socket.on('ledAzul', () => {
+			let colorAnterior = "#4b4bf7";
+			ledAzul.style.backgroundColor = 'blue';
+			setTimeout(() => {
+				ledAzul.style.backgroundColor = colorAnterior;
+			},2000);	
+		});
+		socket.on('ledVerde', () => {
+			let colorAnterior = "#5fd85f";
+			ledVerde.style.backgroundColor = 'green';
+			setTimeout(() => {
+				ledVerde.style.backgroundColor = colorAnterior;
+			},2000);
+			
+		});
+		socket.on('ledAmarillo', () => {
+			let colorAnterior = "#e4e495";
+			ledAmarillo.style.backgroundColor = 'yellow';
+			setTimeout(() => {
+				ledAmarillo.style.backgroundColor = colorAnterior;
+			}, 2000);
+		});
+	}
+
+	function takePhoto(){
+
+	}
 }
+
+
 
 window.onload = leer;
--- websockets/base.js
+++ websockets/base.js
@@ -6,6 +6,11 @@ const port = new SerialPort('COM5', {
 
 const parser = port.pipe(new Readline({delimiter: '\r\n'}));
 
+const delay = (ms) => {
+	const startPoint = new Date().getTime()
+	while (new Date().getTime() - startPoint <= ms) {/* wait */}
+}
+
 /*var five = require("johnny-five");
 var board = new five.Board();
 */
@@ -24,6 +29,12 @@ module.exports = function (io){
 			}
 			else if(data == "noJugando"){
 				jugando = data;
+			}
+			else if(data == "apagado" || data == "encendido"){
+				if(data == "apagado")
+					io.emit("estado", "Encender");
+				else
+					io.emit("estado", "Apagar");
 			}
 			else if(data == "ledRojo"){
 				io.emit("ledRojo");
@@ -64,6 +75,8 @@ module.exports = function (io){
 			}
 		});
 
+		setInterval(prueba, 5000);
+
 		cliente.on("disconnect", data => {
 			console.log("Cliente Desconectado: " + cliente.request.connection.remoteAddress);
 			console.log("Motivo: " + data);
@@ -71,5 +84,73 @@ module.exports = function (io){
 
 	});
 
-
+	function prueba(){
+		parser.emit("data", "jugando");
+		setTimeout(() => {
+			parser.emit("data", "ledAzul");
+			setTimeout(() => {
+				parser.emit("data", "ledVerde");
+				setTimeout(() => {
+					parser.emit("data", "ledAmarillo");
+					setTimeout(() => {
+						parser.emit("data", 4);
+						setTimeout(() => {
+							parser.emit("data", "ledAzul");
+							setTimeout(() => {
+								parser.emit("data", "ledVerde");
+								setTimeout(() => {
+									parser.emit("data", "ledAzul");
+									setTimeout(() => {
+										parser.emit("data", "ledVerde");
+										setTimeout(() => {
+											parser.emit("data", "ledRojo");
+											setTimeout(() => {
+												parser.emit("data", "ledRojo");
+												setTimeout(() => {
+													parser.emit("data", 5);
+													setTimeout(() => {
+														parser.emit("data", "ledVerde");
+														setTimeout(() => {
+															parser.emit("data", "ledAzul");
+															setTimeout(() => {
+																parser.emit("data", "ledVerde");
+																setTimeout(() => {
+																	parser.emit("data", "ledRojo");
+																	setTimeout(() => {
+																		parser.emit("data", "ledRojo");
+																		setTimeout(() => {
+																			parser.emit("data", "ledAmarillo");
+																			setTimeout(() => {
+																				parser.emit("data", "ledAmarillo");
+																				setTimeout(() => {
+																					parser.emit("data", 4);
+																					setTimeout(() => {
+																						parser.emit("data", "apagado");
+																						setTimeout(() => {
+																							parser.emit("data", "encendido");														
+																							setTimeout(() => {
+																								parser.emit("data", "noJugando");
+																							},2000);
+																						},2000);														
+																					},2000);											
+																				},2000);
+																			},2000);
+																		},2000);
+																	},2000);
+																},2000);
+															},2000);
+														},2000);
+													},2000);
+												},2000);
+											},2000);
+										},2000);
+									},2000);
+								},2000);
+							},2000);
+						},2000);
+					},2000);
+				},2000);
+			},2000);
+		},2000);		
+	}
 };
